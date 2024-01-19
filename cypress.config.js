b6c980403463cdaf8cefb7cd96500e4925d59cd8
Daniel Oliveira Alves <39285325+Alves-Daniel@users.noreply.github.com>
2024-01-19 12:04:06 -0400

    Update cypress.config.js
    mudança da resolução da tela

--- cypress.config.js
+++ cypress.config.js
@@ -17,8 +17,8 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    viewportWidth: 1440,
-    viewportHeight: 900,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     baseUrl: "https://buger-eats-qa.vercel.app",
     setupNodeEvents(on, config) {
       // implement node event listeners here
